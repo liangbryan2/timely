64129d205e597a6f6caf922fa2bcf88eee3af139
Onion Rings <liangbryan2@gmail.com>
2018-09-19 22:56:29 -0700

    scary deleting

--- controllers/externalAPI.js
+++ controllers/externalAPI.js
@@ -19,6 +19,9 @@ var books = require('google-books-search');
 
 // //Movie API
 var omdbApi = require('omdb-client');
+var omdb = require("omdb");
+const MovieDb = require('moviedb-promise')
+const moviedb = new MovieDb('91433a6303c384292937cb016af9c4cd');
 
 // ======================================================
 // search for games
@@ -64,51 +67,120 @@ router.get("/games/:query", function (req, res) {
 // search for movies
 // ======================================================
 
-router.get("/movies/:query", function (req, res) {
-  var query = req.params.query;
-  var params = {
-    apiKey: 'trilogy',
-    title: query
-  }
-  omdbApi.get(params, function (err, data) {
-    if (err) {
-      res.status(500);
-    } else {
-      if (data.length) {
-        var loopLength;
-        if (data.length <= 10) {
-          loopLength = data.length;
-        } else if (data.length > 10) {
-          loopLength = 10;
-        }
-        var object = {}
-        var array = [];
-        for (var i = 0; i < loopLength; i++) {
-          var totalMin = parseInt(result[i].Runtime);
-          var movieObj = {
-            name: data[i].title,
-            lengthAttr1: data[i].pageCount,
-            lengthAttr2: '',
-            imgUrl: data[i].thumbnail,
-            apiId: data[i].id,
-            time: totalMin,
-            type: "movies"
-          }
-          array.push(movieObj);
-        }
-        object.items = array;
-      }
-      
-      res.send("test");
-      // res.render("searchresults", object);
-      return;
+// router.get("/movies/:query", function (req, res) {
+
+//   moviedb.searchMovie({
+//     query: req.params.query
+//   }).then(result => {
+//     res.json(result)
+//   }).catch(console.error)
+// })
+
+// var loopLength;
+// if (data.length <= 10) {
+//   loopLength = data.length;
+// } else if (data.length > 10) {
+//   loopLength = 10;
+// }
+//   var object = {}
+//   var array = [];
+//   for (var i = 0; i < loopLength; i++) {
+//     var totalMin = parseInt(data[i].Runtime);
+//     var movieObj = {
+//       name: data[i].title,
+//       lengthAttr1: data[i].pageCount,
+//       lengthAttr2: '',
+//       imgUrl: data[i].thumbnail,
+//       apiId: data[i].id,
+//       time: totalMin,
+//       type: "movies"
+//     }
+//     array.push(movieObj);
+//   }
+//   object.items = array;
+
+// res.render("searchresults", object);
+
+//===========================================================
+// tv shows
+// ==========================================================
+router.get("/shows/:query", function (req, res) {
+  moviedb.searchTv({
+    query: req.params.query
+  }).then(function (data) {
+    var results = data.results;
+    var loopLength;
+    if (results.length <= 10) {
+      loopLength = results.length;
+    } else if (results.length > 10) {
+      loopLength = 10;
     }
+    var ids = [];
+    for (var i = 0; i < loopLength; i++) {
+      ids.push(results[i]);                              
+    }
+    for (var i = 0; i < )
   })
 })
+
+
+// ==========================================================
+// end
+// ==========================================================
+
+
+
+// =============== md-movie-utils ==============================
+
+
+
+let movieDBClients = require('md-movie-utils').clients;
+let omdbClient = movieDBClients.OMDBClient.getInstance('trilogy');
+
+
+
+
+var response = {
+  success: function (res) {
+    return function (movie) {
+      if (movie) {
+        return res.status(200).json(movie);
+      } else {
+        return res.status(404);
+      }
+    };
+  },
+  failure: function (res) {
+    return function (err) {
+      return res.status(500).json({
+        message: err.message
+      });
+    }
+  }
+};
+
+app.get("/movies/:query", function (req, res) {
+  omdbClient.search({query: req.params.query})
+    .then(response.success(res), response.failure(res));
+});
+
+
+
+
+// http: //localhost:3000/omdb/search?q=Batman
+// req.query.q = batman
+// /omdb/movie/getByTitleAndYear?title=Saw&year=2004
+
+
+
 
 // ======================================================
 // end
 // ======================================================
+
+
+// =====================================
+
 
 // var resultsArr = [];
 
@@ -188,6 +260,58 @@ router.get("/movies/:query", function (req, res) {
 //   return whatever(resultsArr);
 // })
 
+router.get("/books/:query", function (req, res) {
+
+  var param = req.params.query;
+  var options = {
+    key: "AIzaSyAgrzMs1uWe7UxhyOHG1lH0u7at5J8C-ic",
+    field: 'title',
+    offset: 0,
+    limit: 10,
+    type: 'books',
+    order: 'relevance',
+    lang: 'en'
+  }
+
+  books.search(param, options, function (error, result) {
+    if (!error) {
+
+      var object = {};
+      var array = [];
+
+      if (result.length) {
+
+        var loopLength;
+        if (result.length <= 10) {
+          loopLength = result.length;
+        } else if (result.length > 10) {
+          loopLength = 10;
+        }
+
+      }
+
+      for (let i = 0; i < loopLength; i++) {
+
+        var totalMin = result[i].pageCount * 2;
+        var bookObj = {
+          name: result[i].title,
+          lengthAttr1: result[i].pageCount,
+          lengthAttr2: '',
+          imgUrl: result[i].thumbnail,
+          apiId: result[i].id,
+          time: totalMin,
+          type: "books"
+        };
+        array.push(bookObj);
+      }
+      object.items = array;
+    } else {
+      console.log(error)
+    }
+    res.render("searchresults", object);
+    return;
+  })
+})
 
 // router.post("/search/books/:query", function(req, res) {
 //   var param = req.params.query;
